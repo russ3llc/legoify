5d94a33c796061e3f11f1977f7a45b8024b48c69
Russell Carpenella <russellcarpenella@gmail.com>
2026-01-13 15:54:06 -0500

    Changes to local util import

--- src/legoify/main.py
+++ src/legoify/main.py
@@ -5,7 +5,14 @@ import numpy as np
 from pathlib import Path
 from skimage import io
 from pyxelate import Pyx, Pal  # https://github.com/sedthh/pyxelate/tree/master
-from util import get_lego_colors, color_name_to_rgb, black_or_white
+
+try:
+    from . import util
+except ImportError:
+    try:
+        from legoify import util
+    except ImportError:
+        import util
 
 parser = argparse.ArgumentParser(
     description="A program to create a mosiac from an image using LEGO colors, and output the part list."
@@ -45,7 +52,7 @@ parser.add_argument(
 # Dither = "none", "naive", "bayer", "floyd", "atkinson" (fastest to slowest)
 def pixelate(image_path, height, width=None, dither=None):
     width = height if width is None else width
-    lego_pal = Pal.from_rgb(get_lego_colors(True))
+    lego_pal = Pal.from_rgb(util.get_lego_colors(True))
     in_image = io.imread(image_path)
     pyx = Pyx(height=height, width=width, palette=lego_pal, dither=dither)
     pyx.fit(in_image)
@@ -60,7 +67,7 @@ def get_color_filter(in_image, color):
         & (in_image[:, :, 2] == color[2])
     )
     mask = np.reshape(np.repeat(mask, 3), np.shape(in_image))
-    bg_color = 0 if black_or_white(color) == [0, 0, 0] else 255
+    bg_color = 0 if util.black_or_white(color) == [0, 0, 0] else 255
     return np.where(mask, in_image, bg_color)
 
 
@@ -85,7 +92,7 @@ def draw_pixel_indicators(in_image, scale=12):
 
 
 def get_part_list(out_im):
-    lego_colors = get_lego_colors()
+    lego_colors = util.get_lego_colors()
     image_colors = out_im.reshape(-1, 3)
     part_list = [
         dict(
@@ -125,7 +132,7 @@ def main():
     if args.color_filters:
         used_colors_names = [p["colorName"] for p in part_list]
         for color_name in used_colors_names:
-            color = color_name_to_rgb(color_name)
+            color = util.color_name_to_rgb(color_name)
             color_filter = get_color_filter(pyx_image, color)
             zoomed_color_filter = get_zoomed_image(color_filter)
             bordered_color_filter = draw_pixel_indicators(zoomed_color_filter)
